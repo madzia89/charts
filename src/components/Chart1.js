f200299e18ec8d1595f109f948d54a8c12d13067
madzia89 <wojcicka.ma@gmail.com>
2018-04-26 21:18:10 +0200

    trying to get different colors on the piechart

--- src/components/Chart1.js
+++ src/components/Chart1.js
@@ -1,34 +1,42 @@
 import React from 'react';
-import {render} from 'react-dom';
 import {PieChart, Pie, Tooltip} from 'recharts'
 
 const data = [
     {
         value: 30,
-        name: 'Weglowodany'
+        name: 'Weglowodany',
+        color: '#35b22a'
     },
     {
         value: 15,
-        name: 'Bialka'
+        name: 'Bialka',
+        color: '#352ab2'
     },
     {
         value: 25,
-        name: 'iTluszcze'
+        name: 'Tluszcze',
+        color: '#c70d33'
     }
 ]
+const styles = {
+    textAlign: 'center',
+    justifyContent: 'center'
+
+}
 
 const Chart1 = () => (
-    <div>
+    <div style={styles}>
+        <h2>Macronutrients in today's meals</h2>
         <PieChart width={500} height={500}>
             <Pie
                 data={data}
                 dataKey="value"
                 nameKey="name"
-                fill="#8884d8"
+                fill= '#352ab2'
             />
             <Tooltip/>
         </PieChart>
     </div>
-);
+)
 
 export default Chart1
