9b38945502a5eef04961e2a1135de7b71c526c08
madzia89 <wojcicka.ma@gmail.com>
2018-05-01 10:57:46 +0200

    randomuser

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Chart1 from './components/Chart1'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Chart1/>
-      </div>
-    );
-  }
+    state = {
+        data: null
+    };
+
+    componentDidMount() {
+        fetch("https://randomuser.me/api?results=5")
+            .then(response => response.json())
+            .then(data => this.setState({data}));
+    }
+
+    render() {
+        return <div>
+            {this.state.data &&
+            (this.state.data).results
+                .map(random => makeUserDiv(random))}
+        </div>;
+    }
 }
+
+const makeUserDiv = (random) => <h1>{random.name.first}</h1>
 
 export default App;
